5ff63c06a7fbefa0c9484e09aa90196044493926
Vicrrs <victorroza22@gmail.com>
2026-08-25 03:27:41 -0400

    feat: adiciona controller de produtos

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"go-api/model"
+	"go-api/usecase"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,23 +9,23 @@ import (
 
 // productController centraliza o tratamento das requisições de produtos.
 type productController struct {
-	// use case
+	productUseCase usecase.ProductUsecase
 }
 
 // NewProductController cria e retorna um controller de produtos.
-func NewProductController() productController {
-	return productController{}
+func NewProductController(usecase usecase.ProductUsecase) productController {
+	return productController{
+		productUseCase: usecase,
+	}
 }
 
 // GetProducts responde com a lista de produtos disponíveis.
 func (p *productController) GetProducts(ctx *gin.Context) {
 
-	products := []model.Product{
-		{
-			ID:    1,
-			Name:  "Batata Frita",
-			Price: 20,
-		},
+	products, err := p.productUseCase.GetProducts()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, err)
 	}
+
 	ctx.JSON(http.StatusOK, products)
 }
